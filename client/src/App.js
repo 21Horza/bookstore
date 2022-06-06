33f0556db0714b72cf892850948e4c93cdd04c22
NEARworld <artemismars2@outlook.com>
2022-06-06 22:44:50 +0900

    style: custom MUI colors
    customizing primary and secondary colors for button component

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,24 @@ import "./App.css";
 import { ThemeProvider } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createTheme } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   const theme = createTheme({
     palette: {
       mode: "dark",
     },
+    primary: {
+      main: "#2196f3",
+    },
+    secondary: {
+      main: "#4caf50",
+    },
   });
 
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Router>
         <Routes>
           <Route path="/" element={<div>Hello world</div>} />
