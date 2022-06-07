a565f58c10f856238f23d3f877dc533f9073d135
NEARworld <artemismars2@outlook.com>
2022-06-07 16:48:51 +0900

    chore: remove axios code from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,9 @@
 import "./App.css";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
 import Navbar from "./components/UI/Navbar";
-import axios from "axios";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      axios
-        .post("/user/login")
-        .then((res) => {
-          if (res.data.success) {
-            setLoggedIn(true);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, [loggedIn]);
-
   const theme = createTheme({
     palette: {
       mode: "dark",
