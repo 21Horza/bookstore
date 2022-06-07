f49b4b4ddedc9098ed2af8ce477c73cfa69b8d02
NEARworld <artemismars2@outlook.com>
2022-06-07 22:07:01 +0900

    feat: setting a global token state

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,21 @@ import "./App.css";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/UI/Navbar";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    console.log(token);
+    console.log(user);
+    if (token) {
+      setUser(true);
+    } else {
+      setUser(false);
+    }
+  }, [user]);
+
   const theme = createTheme({
     palette: {
       mode: "dark",
@@ -20,7 +33,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        <Navbar />
+        <Navbar user={user} setUser={setUser} />
         <Routes>
           <Route path="/" element={<div>Hello world</div>} />
         </Routes>
